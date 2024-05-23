838b3de14a10539c6e828e05940884bfce9f4bcf
jvalladares <jvalladares@teching.com.pe>
2024-05-23 18:45:56 -0500

    interfaz-grafica-web

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const logo_tech = document.querySelector(".logo-teching")
+    setTimeout(()=>{
+        logo_tech.style.opacity = 1;
+        logo_tech.style.left =  0;
+    })
+
+})
